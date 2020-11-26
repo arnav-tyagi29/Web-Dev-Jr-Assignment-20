70b2b02c4def5812b7036adcebb880a52f7978f9
Arnav Tyagi <69686731+arnav-tyagi29@users.noreply.github.com>
2020-11-26 10:38:56 +0530

    Add files via upload

---
+++ Assignment 20/index.js
@@ -0,0 +1,23 @@
+a=  prompt("Your First Name?");
+b=  prompt("Your Last Name?");
+c=  prompt("Your Age?");
+d=  prompt("Your Address?");
+e=  prompt("Your Phone Number?");
+f=  prompt("Your Father's Name?");
+g=  prompt("Your Mother's Name?");
+h=  prompt("Your Photograph link?");
+        
+console.log ("Your First Name:",a);
+console.log( "Your Last Name:",b);
+console.log("Your Age:",c);
+console.log("Your Address:",d);
+console.log("Your Phone Number:",e);
+console.log("Your Father's Name:",f);
+console.log("Your Mother's Name:",g);
+console.log("Your Photograph link:",h);
+       
+        
+       
+        
+       
+       
